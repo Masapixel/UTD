04d5bbd4890b71a4963d92f01b84197303ae5806
Masapixel <amaisalourenco@gmail.com>
2021-05-23 21:45:23 -0300

    Atividade 7

---
+++ atv7/exibir.php
@@ -0,0 +1,53 @@
+<?php
+	$produtos = file("produtos.txt");
+	// echo "<script>console.log($produtos)</script>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Usuários Cadastro</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 offset-md-2">
+				<hr><h4>Lista de Produtos</h4></hr>
+
+				<table class="table">
+					<thead>
+						<th> Nome do Produto </th>
+						<th> Quantidade </th>
+						<th> Preço </th>
+					</thead>
+
+					<tbody>
+						<?php  foreach ($produtos as $produto){
+								$dado = explode(" - ", $produto);
+
+						?>
+						<tr>
+							<td><?php echo $dado[0]; ?></td>
+							<td><?php echo $dado[1]; ?></td>
+							<td><?php echo $dado[2]; ?></td>
+
+						</tr>
+
+						<?php } ?>
+					</tbody>
+					
+				</table>
+				<a href="cadastro.php" class="btn btn-primary"> Cadastrar Produtos</a>				
+				</form>
+			</div>
+		</div>
+	</div>	
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</body>
+</html>
